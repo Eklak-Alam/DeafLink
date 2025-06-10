08f82a41313f1cbb9540396321366fa0f97cf436
Eklak Alam <eklakalam420@gmail.com>
2025-06-10 22:40:09 +0530

    added

--- components/ui/hero-parallax.jsx
+++ components/ui/hero-parallax.jsx
@@ -62,7 +62,7 @@ export const Header = () => {
     <div
       className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white text-white">
-        The Ultimate <br /> Deaf Link
+        Deaf Link
       </h1>
     </div>
   );
